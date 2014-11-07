f54a67971e096dd62347cbc298cd81a753c0d988
Lili Serfaty <lili.serfaty@gmail.com>
2014-11-07 11:16:29 -0600

    add test for categories. response is parsed into category hash and 2 categories are selected for the user

--- spec/web/server_spec.rb
+++ spec/web/server_spec.rb
@@ -115,6 +115,85 @@ describe "Chuser App" do
     end
 
     it 'sorts results by category' do
+      oauth_creds = {
+        consumer_key:    ENV['YELP_KEY'],
+        consumer_secret: ENV['YELP_SECRET'],
+        token:           ENV['YELP_TOKEN'],
+        token_secret:    ENV['YELP_TOKEN_SECRET']
+      }
+      client = Yelpify.new_client(oauth_creds)
+
+      address     = "9704+sydney+marilyn+lane+austin+tx+78748"
+      radius      = "1610"
+      search_data = {
+        "location"      => address,
+        "radius_filter" => radius,
+        "limit"         => 3
+      }
+      search_data["category_filter"] = "food,restaurants"
+      VCR.use_cassette('search') do
+        response = client.search(search_data)
+          categories = {}
+          response.businesses.each_index do |i|
+          business = response.businesses[i]
+          category = business.categories[0][0]
+          if categories[category]
+            categories[category] <<
+              [business.name,
+              business.location.display_address.join(', ').gsub(/,/, '').gsub(/\s/, '+'), 
+              business.url]
+          else categories[category] = [] << 
+              [business.name, 
+              business.location.display_address.join(', ').gsub(/,/, '').gsub(/\s/, '+'), 
+              business.url]
+          end
+        end
+        expect(categories).to_not be_nil
+        expect(categories.length).to eq(3)
+        expect(categories.values[0][0][0]).to be_a(String)
+        expect(categories.values[0][0][1]).to include('+')
+        expect(categories.values[0][0][2]).to include('www')
+      end
+    end
+
+    it 'returns two categories' do
+      oauth_creds = {
+        consumer_key:    ENV['YELP_KEY'],
+        consumer_secret: ENV['YELP_SECRET'],
+        token:           ENV['YELP_TOKEN'],
+        token_secret:    ENV['YELP_TOKEN_SECRET']
+      }
+      client = Yelpify.new_client(oauth_creds)
+
+      address     = "9704+sydney+marilyn+lane+austin+tx+78748"
+      radius      = "1610"
+      search_data = {
+        "location"      => address,
+        "radius_filter" => radius,
+        "limit"         => 3
+      }
+      search_data["category_filter"] = "food,restaurants"
+      VCR.use_cassette('search') do
+        response = client.search(search_data)
+          categories = {}
+          response.businesses.each_index do |i|
+          business = response.businesses[i]
+          category = business.categories[0][0]
+          if categories[category]
+            categories[category] <<
+              [business.name,
+              business.location.display_address.join(', ').gsub(/,/, '').gsub(/\s/, '+'), 
+              business.url]
+          else categories[category] = [] << 
+              [business.name, 
+              business.location.display_address.join(', ').gsub(/,/, '').gsub(/\s/, '+'), 
+              business.url]
+          end
+        end
+        cats = categories.to_a.sample(2)
+        choices = Hash[cats].keys
+        expect(choices.length).to eq(2)
+      end
     end
 
   end
